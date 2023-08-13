eb269ca2851e221896fc6fe402ca5c8bde6e1542
patfb <snailmail>
2023-08-12 21:40:52 -0400

    739. Daily Temperatures brute force approach

---
+++ src/stack/739_dailyTemperatures/bruteForceNSquared.ts
@@ -0,0 +1,22 @@
+const dailyTemperatures = (temps: number[]): number[] => {
+  const originalTemps = temps.map((temp, index) => ({
+    temp,
+    index,
+  }));
+
+  const tempsWithIndices = [...originalTemps];
+
+  const result = tempsWithIndices.map((today) => {
+    const firstWarmerDay = originalTemps.find(
+      (originalTemp) =>
+        originalTemp.temp > today.temp && originalTemp.index > today.index,
+    );
+
+    if (firstWarmerDay === undefined) return 0; // we couldn't find a warmer day
+    return firstWarmerDay.index - today.index; // index diff between warmer day and today
+  });
+
+  return result;
+};
+
+console.log("daily temperatures", dailyTemperatures([30, 60, 90]));
