cbcaed73fc486e6daa6272497dfda77d40eef270
patfb <snailmail>
2024-06-10 20:57:49 -0400

    valid parentheses progress

---
+++ src/stack/20_validParentheses/__tests__/validParentheses.js
@@ -0,0 +1,12 @@
+// import { describe, expect, it } from "@jest/globals";
+// import { isValid } from "../validParentheses";
+
+// describe("validParentheses", () => {
+//   it.each([
+//     // { s: "()", expected: true },
+//     { s: "()[]{}", expected: true },
+//     // { s: "(]", expected: false },
+//   ])("$s -> $expected", ({ s, expected }) => {
+//     expect(isValid(s)).toStrictEqual(expected);
+//   });
+// });
---
+++ src/stack/20_validParentheses/validParentheses.js
@@ -0,0 +1,22 @@
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// const isValid = (s) => {
+//   const map = new Map();
+
+//   map.set(")", "(");
+//   map.set("]", "[");
+//   map.set("}", "{");
+
+//   const sArray = s.split("");
+//   const stack = [];
+
+//   while (sArray.length) {
+//     stack.push(sArray.shift());
+//   }
+
+//   console.log("stack", stack);
+// };
+
+// export { isValid };
