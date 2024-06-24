2a99ef61f6589d494c524a80f30a679fa077ac99
patfb <snailmail>
2024-06-24 18:48:26 -0400

    rotate array progress

--- src/generic/189_rotateArray/__tests__/rotateArray.test.js
+++ src/generic/189_rotateArray/__tests__/rotateArray.test.js
@@ -1,11 +1,22 @@
 import { describe, expect, it } from "@jest/globals";
-import { rotate } from "../rotateArray";
+import { rotate, rotateRecursiveApproach } from "../rotateArray";
 
-describe("rotate", () => {
-  it.each([
-    { nums: [1, 2, 3, 4, 5, 6, 7], k: 3, expected: [5, 6, 7, 1, 2, 3, 4] },
-    // { nums: [-1, -100, 3, 99], k: 2, expected: [3, 99, -1, -100] },
-  ])("$nums, $k -> $expected", ({ nums, k, expected }) => {
-    expect(rotate(nums, k)).toStrictEqual(expected);
+describe("189. Rotate Array", () => {
+  describe("recursive approach", () => {
+    it.each([
+      { nums: [1, 2, 3, 4, 5, 6, 7], k: 3, expected: [5, 6, 7, 1, 2, 3, 4] },
+      { nums: [-1, -100, 3, 99], k: 2, expected: [3, 99, -1, -100] },
+    ])("$nums, $k -> $expected", ({ nums, k, expected }) => {
+      expect(rotateRecursiveApproach(nums, k)).toStrictEqual(expected);
+    });
+  });
+
+  describe.only("map approach", () => {
+    it.each([
+      { nums: [1, 2, 3, 4, 5, 6, 7], k: 3, expected: [5, 6, 7, 1, 2, 3, 4] },
+      // { nums: [-1, -100, 3, 99], k: 2, expected: [3, 99, -1, -100] },
+    ])("$nums, $k -> $expected", ({ nums, k, expected }) => {
+      expect(rotateRecursiveApproach(nums, k)).toStrictEqual(expected);
+    });
   });
 });
--- src/generic/189_rotateArray/rotateArray.js
+++ src/generic/189_rotateArray/rotateArray.js
@@ -19,4 +19,27 @@ const rotate = (nums, k) => {
   return result;
 };
 
-export { rotate };
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotateRecursiveApproach = (nums, k) => {
+  const numLength = nums.length;
+
+  const numsWithIndex = nums.map((value, index) => ({
+    value,
+    originalIndex: index,
+    indexPlusK: index + k,
+  }));
+
+  const cyclicValues = numsWithIndex.filter(
+    (num) => num.indexPlusK > numLength - 1,
+  );
+
+  console.log("numLength", numLength);
+  console.log("numsWithIndex", numsWithIndex);
+  console.log("cyclicValues", cyclicValues);
+};
+
+export { rotate, rotateRecursiveApproach };
